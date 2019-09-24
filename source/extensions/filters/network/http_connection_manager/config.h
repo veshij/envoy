2cb8401d4cf4782f364deed5601798e3391b840b
Oleg Guba <oleg@dropbox.com>
2019-09-24 04:59:25 +0000

    propagate lifetime timeout
    Signed-off-by: Oleg Guba <oleg@dropbox.com>

--- source/extensions/filters/network/http_connection_manager/config.h
+++ source/extensions/filters/network/http_connection_manager/config.h
@@ -108,6 +108,7 @@ public:
   uint32_t maxRequestHeadersKb() const override { return max_request_headers_kb_; }
   uint32_t maxRequestHeadersCount() const override { return max_request_headers_count_; }
   absl::optional<std::chrono::milliseconds> idleTimeout() const override { return idle_timeout_; }
+  absl::optional<std::chrono::milliseconds> connectionsLifetimeTimeout() const override { return connection_lifetime_timeout_; }
   std::chrono::milliseconds streamIdleTimeout() const override { return stream_idle_timeout_; }
   std::chrono::milliseconds requestTimeout() const override { return request_timeout_; }
   Router::RouteConfigProvider* routeConfigProvider() override {
@@ -178,6 +179,7 @@ private:
   const uint32_t max_request_headers_kb_;
   const uint32_t max_request_headers_count_;
   absl::optional<std::chrono::milliseconds> idle_timeout_;
+  absl::optional<std::chrono::milliseconds> connection_lifetime_timeout_;
   std::chrono::milliseconds stream_idle_timeout_;
   std::chrono::milliseconds request_timeout_;
   Router::RouteConfigProviderPtr route_config_provider_;
